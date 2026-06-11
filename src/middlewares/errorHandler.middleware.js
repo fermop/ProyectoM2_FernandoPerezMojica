c5035da484e971a8868475dc56ba7bf69d0e8700
Fernando Pérez Mojica <fernandoperezmojica@gmail.com>
2026-06-10 18:46:03 -0600

    fix: devuelve correctamente el statusCode

--- src/middlewares/errorHandler.middleware.js
+++ src/middlewares/errorHandler.middleware.js
@@ -6,6 +6,6 @@ export default function errorHandlerMiddleware(err, req, res, next) {
   return res.status(statusCode).json({
     status: 'error',
     message: err.message || 'Ocurrió un error inesperado en el servidor.',
-    code: err.code || 'INTERNAL_SERVER_ERROR'
+    code: err.statusCode || 'INTERNAL_SERVER_ERROR'
   })
 }
